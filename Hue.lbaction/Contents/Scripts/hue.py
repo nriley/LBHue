baf94b447a7ab5acac14e5e28dfd4cec9bbff195
Nicholas Riley <com-github@sabi.net>
2016-07-20 09:29:42 -0400

    Use hue icons.

--- Hue.lbaction/Contents/Scripts/hue.py
+++ Hue.lbaction/Contents/Scripts/hue.py
@@ -4,7 +4,48 @@ __all__ = ('lights', 'light', 'item_for_light', 'toggle_item_for_light',
            'group', 'scenes_item_for_group',
            'scenes', 'scene', 'item_for_scene_name')
 
-# XXX replace this with either nothing or something that isn't GPLv2
+HUE_PRODUCTS = dict(
+    LLC014='aura',
+    HBL001='beyond_ceiling_pendant_table',
+    HBL002='beyond_ceiling_pendant_table',
+    HBL003='beyond_ceiling_pendant_table',
+    LLC005='bloom',
+    LLC011='bloom',
+    LLC012='bloom',
+    LLC007='bloom',
+    LCT002='br30',
+    LCT011='br30',
+    LTW011='br30',
+    LWB005='br30',
+    LWB011='br30',
+    HEL001='entity',
+    HEL002='entity',
+    LLC020='go',
+    LCT003='gu10_par16',
+    HIL001='impulse',
+    HIL002='impulse',
+    LLC006='iris',
+    LLC010='iris',
+    LST001='lightstrip',
+    LST002='lightstrip',
+    HML001='phoenix_ceiling_pendant_table_wall',
+    HML002='phoenix_ceiling_pendant_table_wall',
+    HML003='phoenix_ceiling_pendant_table_wall',
+    HML004='phoenix_ceiling_pendant_table_wall',
+    HML005='phoenix_ceiling_pendant_table_wall',
+    HML006='phoenix_ceiling_pendant_table_wall',
+    HML007='phoenix_recessed_spot',
+    LLC013='storylight',
+    LCT001='white_and_color_e27_b22',
+    LCT007='white_and_color_e27_b22',
+    LCT010='white_and_color_e27_b22',
+    LWT010='white_and_color_e27_b22',
+    LWB004='white_and_color_e27_b22',
+    LWB006='white_and_color_e27_b22',
+    LWB010='white_e27_b22',
+    LWB014='white_e27_b22')
+
+# XXX replace qhue with either nothing or something that isn't GPLv2
 import collections, qhue, urllib
 
 bridge = qhue.Bridge('192.168.0.14', 'USERNAME')
@@ -19,9 +60,12 @@ def light(light_id):
     return bridge.lights[light_id]
 
 def item_for_light(light_id, light_info):
+    icon = 'font-awesome:fa-lightbulb-o'
+    if light_info['manufacturername'] == 'Philips':
+        icon = HUE_PRODUCTS.get(light_info['modelid'], icon)
     return dict(
         title=light_info['name'],
-        icon='font-awesome:fa-lightbulb-o',
+        icon=icon,
         iconIsTemplate=True)
 
 def action_url(kind, **params):
@@ -34,7 +78,6 @@ def toggle_item_for_light(light_id, light_info):
     want_on = 0 if on else 1
     item.update(
         action='action.py',
-        actionReturnsItems=True,
         url=action_url('light', id=light_id, on=want_on))
     if on:
         item['badge'] = 'ON'
@@ -77,13 +120,11 @@ def scene(scene_id):
 def item_for_scene_name(scene_name):
     return dict(
         title=scene_name,
-        icon='font-awesome:picture-o',
-        iconIsTemplate=True)
+        icon='font-awesome:picture-o')
 
 def set_item_for_scene(group_id, (scene_name, scene_id)):
     item = item_for_scene_name(scene_name)
     item.update(
         action='action.py',
-        actionReturnsItems=True,
         url=action_url('group', id=group_id, scene=scene_id))
     return item
