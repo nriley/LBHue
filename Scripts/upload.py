11b86ecc9bc6bc301692133be211d483bc0f4187
Nicholas Riley <com-github@sabi.net>
2016-07-23 15:43:58 -0400

    Compile scripts; fix whitespace in upload script.

--- Scripts/upload.py
+++ Scripts/upload.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import argparse
+import compileall
 import distutils.version
 import json
 import os
@@ -10,6 +11,10 @@ import sys
 import urllib
 import webbrowser
 
+def compile_scripts(bundle_path):
+    scripts_path = os.path.join(bundle_path, 'Contents', 'Scripts')
+    compileall.compile_dir(scripts_path, maxlevels=0)
+
 def update_bundle_version(bundle_path, version):
     info_plist_path = os.path.join(bundle_path, 'Contents', 'Info.plist')
     info_plist = plistlib.readPlist(info_plist_path)
@@ -17,7 +22,8 @@ def update_bundle_version(bundle_path, version):
     plistlib.writePlist(info_plist, info_plist_path)
 
 def sign_bundle(bundle_path):
-    subprocess.check_call(['/usr/bin/codesign', '-fs', 'Developer ID Application: Nicholas Riley',
+    subprocess.check_call(['/usr/bin/codesign', '-fs',
+                           'Developer ID Application: Nicholas Riley',
                            bundle_path])
 
 def archive_bundle(bundle_path, version):
@@ -25,8 +31,10 @@ def archive_bundle(bundle_path, version):
     bundle_name, bundle_ext = os.path.splitext(bundle_filename)
     # GitHub will replace spaces with periods; dashes look better
     bundle_name = bundle_name.replace(' ', '-')
-    archive_path = os.path.join(dir_path, '%s-%s%s' % (bundle_name, version, bundle_ext))
-    subprocess.check_call(['/usr/bin/ditto', '--keepParent', '-ck', bundle_path, archive_path])
+    archive_path = os.path.join(dir_path, '%s-%s%s' %
+                                (bundle_name, version, bundle_ext))
+    subprocess.check_call(['/usr/bin/ditto', '--keepParent', '-ck',
+                           bundle_path, archive_path])
 
     return archive_path
 
@@ -41,25 +49,29 @@ def expand_url_template(url_template, query=None, *args):
 def upload_release(repo, version, archive_path, github_access_token):
     strict_version = distutils.version.StrictVersion(version)
 
-    releases_url = expand_url_template('https://api.github.com/repos/%s/releases',
-                                       dict(access_token=github_access_token), repo)
+    releases_url = expand_url_template(
+        'https://api.github.com/repos/%s/releases',
+        dict(access_token=github_access_token), repo)
 
     release_name = 'v' + version
     release_json = dict(tag_name=release_name, target_commitish='master',
                         name=release_name, body='', draft=True,
                         prerelease=bool(strict_version.prerelease))
 
-    releases_api = subprocess.Popen(['/usr/bin/curl', '--data', '@-', releases_url],
-                                    stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    releases_api = subprocess.Popen(
+        ['/usr/bin/curl', '--data', '@-', releases_url],
+        stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     release_json_data, _ = releases_api.communicate(json.dumps(release_json))
     release_json = json.loads(release_json_data)
 
     html_url = release_json['html_url']
     upload_url = release_json['upload_url'].split('{', 1)[0]
-    upload_url = expand_url_template(upload_url, dict(name=os.path.basename(archive_path),
-                                                      access_token=github_access_token))
-    subprocess.check_call(['/usr/bin/curl', '-H', 'Content-Type: application/zip',
-                           '--data-binary', '@' + archive_path, upload_url])
+    upload_url = expand_url_template(
+        upload_url, dict(name=os.path.basename(archive_path),
+                         access_token=github_access_token))
+    subprocess.check_call(
+        ['/usr/bin/curl', '-H', 'Content-Type: application/zip',
+         '--data-binary', '@' + archive_path, upload_url])
 
     return html_url
 
@@ -67,11 +79,13 @@ def release(version, github_access_token):
     project_path = os.path.join(os.path.dirname(__file__), '..')
     action_path = os.path.join(project_path, 'Hue.lbaction')
 
+    compile_scripts(action_path)
     update_bundle_version(action_path, version)
     sign_bundle(action_path)
 
     archive_path = archive_bundle(action_path, version)
-    html_url = upload_release('nriley/LBHue', version, archive_path, github_access_token)
+    html_url = upload_release('nriley/LBHue', version, archive_path,
+                              github_access_token)
 
     webbrowser.open(html_url)
 
